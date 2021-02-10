c93cd0b11e9a243699ec9ae63aa729535f2e7a4d
Valeriy Shagur <vshagur@gmail.com>
2021-02-10 12:46:46 +0700

    [refactor] refactor keyboard

--- vkbot/keyboard.py
+++ vkbot/keyboard.py
@@ -1,35 +1,105 @@
 import json
 
+# NOTE: Keyboard does not support inline mode
+#
+# Spied on here:
+# https://github.com/python273/vk_api/blob/master/vk_api/keyboard.py
+# Thank you, guys :)
+#
+
+
+from enum import Enum
+
+MAX_BUTTONS_ON_LINE = 5
+MAX_DEFAULT_LINES = 10
+MAX_INLINE_LINES = 6
+
+
+class VkKeyboardColor(Enum):
+    """ Возможные цвета кнопок """
+
+    #: Синяя
+    PRIMARY = 'primary'
+
+    #: Белая
+    SECONDARY = 'secondary'
+
+    #: Красная
+    NEGATIVE = 'negative'
+
+    #: Зелёная
+    POSITIVE = 'positive'
+
+
+class VkKeyboardButton(Enum):
+    """ Возможные типы кнопки """
+
+    #: Кнопка с текстом
+    TEXT = "text"
+
 
 class VkKeyboard(object):
-    MAX_BUTTONS = 10
+    """ Класс для создания клавиатуры для бота (https://vk.com/dev/bots_docs_3)
+    :param one_time: Если True, клавиатура исчезнет после нажатия на кнопку
+    :type one_time: bool
+    """
 
-    def __init__(self, one_time=True):
+    __slots__ = ('one_time', 'lines', 'keyboard', 'inline')
+
+    def __init__(self, one_time=False):
         self.one_time = one_time
-        self.buttons = []
+        self.lines = [[]]
 
         self.keyboard = {
             'one_time': self.one_time,
-            'buttons': self.buttons
+            'inline': False,
+            'buttons': self.lines
         }
 
     def get_keyboard(self):
+        """ Получить json клавиатуры """
         return json.dumps(self.keyboard)
 
-    def add_text_button(self, color, label, payload):
-        if len(self.buttons) >= self.MAX_BUTTONS:
-            raise ValueError(
-                f'The number of buttons should not exceed {self.MAX_BUTTONS}.'
-            )
+    @classmethod
+    def get_empty_keyboard(cls):
+        """ Получить json пустой клавиатуры.
+        Если отправить пустую клавиатуру, текущая у пользователя исчезнет.
+        """
+        keyboard = cls()
+        keyboard.keyboard['buttons'] = []
+        return keyboard.get_keyboard()
 
-        self.buttons.append([{
-            'color': color,
+    def add_button(self, label, color=VkKeyboardColor.SECONDARY, payload=None):
+        """ Добавить кнопку с текстом.
+            Максимальное количество кнопок на строке - MAX_BUTTONS_ON_LINE
+        :param label: Надпись на кнопке и текст, отправляющийся при её нажатии.
+        :type label: str
+        :param color: цвет кнопки.
+        :type color: VkKeyboardColor or str
+        :param payload: Параметр для callback api
+        :type payload: str or list or dict
+        """
+
+        current_line = self.lines[-1]
+
+        if len(current_line) >= MAX_BUTTONS_ON_LINE:
+            raise ValueError(f'Max {MAX_BUTTONS_ON_LINE} buttons on a line')
+
+        color_value = color
+
+        if isinstance(color, VkKeyboardColor):
+            color_value = color_value.value
+
+        if payload is not None:
+            payload = json.dumps(payload)
+
+        button_type = VkKeyboardButton.TEXT.value
+
+        current_line.append({
+            'color': color_value,
             'action': {
-                'type': 'text',
+                'type': button_type,
                 'payload': payload,
                 'label': label,
             }
-        }, ])
-
-
-
+        })
