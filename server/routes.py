7df718f723ac7dcd090bf34f6b2521380740c39c
Valeriy Shagur <vshagur@gmail.com>
2021-02-13 12:34:15 +0700

    [chore] add server structure

---
+++ server/routes.py
@@ -0,0 +1,6 @@
+# server/routes.py
+from views import index
+
+
+def setup_routes(app):
+    app.router.add_get('/', index)
---
+++ server/views.py
@@ -0,0 +1,6 @@
+# server/views.py
+from aiohttp import web
+
+
+async def index(request):
+    return web.Response(text='Hello from server!')
