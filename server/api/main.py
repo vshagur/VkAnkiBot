79799e00f1e46a7ca338b9063915ed7079964f96
Valeriy Shagur <vshagur@gmail.com>
2021-02-13 21:05:15 +0700

    [chore] add correct setting for postgresql

--- server/api/main.py
+++ server/api/main.py
@@ -1,10 +1,18 @@
 # server/api/main.py
 from aiohttp import web
 
+from logger import logger
 from routes import setup_routes
 from settings import config
 
-app = web.Application()
-setup_routes(app)
-app['config'] = config
-web.run_app(app)
+
+def main():
+    app = web.Application()
+    setup_routes(app)
+    app['config'] = config
+    logger.debug(f'Load config: {config}')
+    web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
