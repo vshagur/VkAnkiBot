e9c2e9cbca9520d41aa2d110779242cf324b3024
Valeriy Shagur <vshagur@gmail.com>
2021-02-14 22:05:41 +0700

    [refactor] refactor api client

--- vkbot/api_client.py
+++ vkbot/api_client.py
@@ -20,83 +20,55 @@ class ApiClient:
         url = urljoin(self.url, '/users')
         data = {'vk_user_id': vk_user_id}
 
-        async with self.session.post(url, json=data) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return await self.make_request(self.session.post(url, json=data))
 
     async def get_help(self):
         url = urljoin(self.url, '/docs/help')
 
-        async with self.session.get(url) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return await self.make_request(self.session.get(url))
 
     async def get_top_players(self):
         url = urljoin(self.url, '/top')
 
-        async with self.session.get(url) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return await self.make_request(self.session.get(url))
 
     async def create_new_game(self, chat_id, vk_user_id):
         url = urljoin(self.url, '/game')
         data = {'chat_id': chat_id, 'vk_user_id': vk_user_id}
 
-        async with self.session.post(url, json=data) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return await self.make_request(self.session.post(url, json=data))
 
     async def update_game_info(self, payload):
         url = urljoin(self.url, '/game')
 
-        async with self.session.put(url, json=payload) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return await self.make_request(self.session.put(url, json=payload))
 
     async def get_quiz(self):
         url = urljoin(self.url, '/question')
 
-        async with self.session.get(url) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return await self.make_request(self.session.get(url))
 
     async def get_result(self, game_id):
         url = urljoin(self.url, f'/results/{game_id}')
+        data = await self.make_request(self.session.get(url))
+        game_id = data.get('game_id')
+        users = data.get('users')
+        score = data.get('score')
+        text = f'The result of the game is {game_id}.\n'
+        text += '\n'.join([f'{num}. {user}' for num, user in enumerate(users, 1)])
+        text += f'\nPoints scored per game: {score}.'
 
-        async with self.session.get(url) as resp:
-            if resp.status == 200:
-                data = await resp.json()
-
-                game_id = data.get('game_id')
-                users = data.get('users')
-                score = data.get('score')
-
-                text = f'The result of the game is {game_id}.\n'
-                text += '\n'.join([f'{num}. {user}' for num, user in enumerate(users, 1)])
-                text += f'\nPoints scored per game: {score}.'
-
-                return text
-
-            else:
-                logger.error('BAD_RESPONSE_STATUS_CODE: {resp.status} from {resource}')
+        return text
 
     async def save_round_info(self, game_id, round_id, winner):
         url = urljoin(self.url, f'/rounds/')
         data = {'vk_user_id': winner, 'game_id': game_id, 'round_id': round_id}
 
-        async with self.session.post(url, json=data) as resp:
+        return await self.make_request(self.session.post(url, json=data))
+
+    async def make_request(self, coroutine):
+
+        async with coroutine as resp:
             if resp.status == 200:
                 return await resp.json()
             else:
