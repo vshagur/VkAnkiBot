56f7a7a54097192d6e35918cb3719cbf319adaf0
Valeriy Shagur <vshagur@gmail.com>
2021-02-16 11:56:33 +0700

    [refactor] rewrite views in class style

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,22 +1,20 @@
 # server/api/routes.py
 from views import (
-    docs,
-    add_user,
-    add_game,
-    update_game_info,
-    get_top_players,
-    get_game_result,
-    get_question,
-    add_round,
+    DocumentView,
+    GameView,
+    UserView,
+    ResultView,
+    RoundView,
+    QuestionView,
+    TopView
 )
 
 
 def setup_routes(app):
-    app.router.add_get('/docs/{name}', docs)
-    app.router.add_get('/results/{game_id}', get_game_result)
-    app.router.add_post('/users', add_user)
-    app.router.add_post('/rounds', add_round)
-    app.router.add_post('/game', add_game)
-    app.router.add_put('/game', update_game_info)
-    app.router.add_get('/top', get_top_players)
-    app.router.add_get('/question', get_question)
+    app.router.add_view('/docs/{name}', DocumentView)
+    app.router.add_view('/game', GameView)
+    app.router.add_view('/users', UserView)
+    app.router.add_view('/results/{game_id}', ResultView)
+    app.router.add_view('/rounds', RoundView)
+    app.router.add_view('/top', TopView)
+    app.router.add_view('/question', QuestionView)
--- server/api/views.py
+++ server/api/views.py
@@ -39,117 +39,130 @@ QUESTIONS = (
 )
 
 
-async def docs(request):
-    name = request.match_info['name']
+class DocumentView(web.View):
 
-    if name:
+    async def get(self):
+        name = self.request.match_info['name']
+
+        if name:
+            # TODO: add logic
+            # created vshagur@gmail.com, 2021-02-14
+            logger.debug('get help page from db')
+            data = {'text': 'very long help page ... from db'}
+            return web.json_response(data)
+
+        raise web.HTTPNotFound()
+
+
+class UserView(web.View):
+
+    async def post(self):
+        data = await self.request.json()
+        vk_user_id = data.get('vk_user_id')
+
+        if vk_user_id:
+            logger.debug(f'add user: {vk_user_id} to db')
+            # TODO: add logic
+            # created vshagur@gmail.com, 2021-02-14
+            # проверить сначала, что такого пользователя еще нет
+            data = {'user_id': 1111111}  # dummy
+
+            return web.json_response(data)
+
+        raise web.HTTPBadRequest()
+
+
+class TopView(web.View):
+
+    async def get(self):
         # TODO: add logic
         # created vshagur@gmail.com, 2021-02-14
-        logger.debug('get help page from db')
-        data = {'text': 'very long help page ... from db'}
-        return web.json_response(data)
-
-    raise web.HTTPNotFound()
-
-
-async def add_user(request):
-    data = await request.json()
-    vk_user_id = data.get('vk_user_id')
-
-    if vk_user_id:
-        logger.debug(f'add user: {vk_user_id} to db')
-        # TODO: add logic
-        # created vshagur@gmail.com, 2021-02-14
-        # проверить сначала, что такого пользователя еще нет
-        data = {'user_id': 1111111}  # dummy
-
-        return web.json_response(data)
-
-    raise web.HTTPBadRequest()
-
-
-async def add_game(request):
-    data = await request.json()
-    chat_id = data.get('chat_id')
-    vk_user_id = data.get('vk_user_id')
-
-    if vk_user_id and chat_id:
-        # TODO: add logic
-        # created vshagur@gmail.com, 2021-02-14
-        game_id = randint(10 ** 4, 10 ** 5 - 1)
-        data = {'game_id': game_id}  # dummy
-
-        logger.debug(f'new game created: {game_id} to db')
-        return web.json_response(data)
-
-    raise web.HTTPBadRequest()
-
-
-async def update_game_info(request):
-    data = await request.json()
-    game_id = data.get('game_id')
-    status = data.get('status')
-
-    if game_id:
-        # TODO: add logic
-        # created vshagur@gmail.com, 2021-02-14
-        data = {
-            'game_id': game_id,
-            # 'vk_user_id': 'vk_user_id from db',
-            # 'chat_id': 'chat_id from db',
-            'status': status,
-        }  # dummy
-
-        logger.debug(f'game info updated: {data}')
-        return web.json_response(data)
-
-    raise web.HTTPBadRequest()
-
-
-async def get_top_players(request):
-    # TODO: add logic
-    # created vshagur@gmail.com, 2021-02-14
-    data = {'users': {'Ivan': 20, 'Oleg': 19}}
-    logger.debug(f'get top players from db: {data}')
-
-    return web.json_response(data)
-
-
-async def get_game_result(request):
-    game_id = request.match_info['game_id']
-
-    if game_id:
-        # TODO: add logic
-        # created vshagur@gmail.com, 2021-02-14
-        data = {'game_id': game_id, 'users': ['Ivan', 'Oleg'], 'score': randint(2, 4)}
-        logger.debug(f'get game result from db: {data}')
+        data = {'users': {'Ivan': 20, 'Oleg': 19}}
+        logger.debug(f'get top players from db: {data}')
 
         return web.json_response(data)
 
-    raise web.HTTPNotFound()
+
+class GameView(web.View):
+
+    async def post(self):
+        data = await self.request.json()
+        chat_id = data.get('chat_id')
+        vk_user_id = data.get('vk_user_id')
+
+        if vk_user_id and chat_id:
+            # TODO: add logic
+            # created vshagur@gmail.com, 2021-02-14
+            game_id = randint(10 ** 4, 10 ** 5 - 1)
+            data = {'game_id': game_id}  # dummy
+
+            logger.debug(f'new game created: {game_id} to db')
+            return web.json_response(data)
+
+        raise web.HTTPBadRequest()
+
+    async def put(self):
+        data = await self.request.json()
+        game_id = data.get('game_id')
+        status = data.get('status')
+
+        if game_id:
+            # TODO: add logic
+            # created vshagur@gmail.com, 2021-02-14
+            data = {
+                'game_id': game_id,
+                # 'vk_user_id': 'vk_user_id from db',
+                # 'chat_id': 'chat_id from db',
+                'status': status,
+            }  # dummy
+
+            logger.debug(f'game info updated: {data}')
+            return web.json_response(data)
+
+        raise web.HTTPBadRequest()
 
 
-async def get_question(request):
-    # TODO: add logic
-    # created vshagur@gmail.com, 2021-02-14
-    data = choice(QUESTIONS)
-    logger.debug(f'get question from db: {data}')
+class ResultView(web.View):
 
-    return web.json_response(data)
+    async def get(self):
+        game_id = self.request.match_info['game_id']
+
+        if game_id:
+            # TODO: add logic
+            # created vshagur@gmail.com, 2021-02-14
+            data = {'game_id': game_id, 'users': ['Ivan', 'Oleg'], 'score': randint(2, 4)}
+            logger.debug(f'get game result from db: {data}')
+
+            return web.json_response(data)
+
+        raise web.HTTPNotFound()
 
 
-async def add_round(request):
-    data = await request.json()
-    vk_user_id = data.get('vk_user_id')
-    game_id = data.get('game_id')
-    round_id = data.get('round_id')
+class QuestionView(web.View):
 
-    if vk_user_id and game_id and round_id:
+    async def get(self):
         # TODO: add logic
         # created vshagur@gmail.com, 2021-02-14
-        data = {'round_id': randint(10 ** 3, 10 ** 4 - 1)}  # dummy
-        logger.debug(f'add round: {round_id} to db')
+        data = choice(QUESTIONS)
+        logger.debug(f'get question from db: {data}')
 
         return web.json_response(data)
 
-    raise web.HTTPBadRequest()
+
+class RoundView(web.View):
+
+    async def post(self):
+        data = await self.request.json()
+        vk_user_id = data.get('vk_user_id')
+        game_id = data.get('game_id')
+        round_id = data.get('round_id')
+
+        if vk_user_id and game_id and round_id:
+            # TODO: add logic
+            # created vshagur@gmail.com, 2021-02-14
+            data = {'round_id': randint(10 ** 3, 10 ** 4 - 1)}  # dummy
+            logger.debug(f'add round: {round_id} to db')
+
+            return web.json_response(data)
+
+        raise web.HTTPBadRequest()
