b00d0e4a6cdad3803aa1d192932b3954d923a10c
Valeriy Shagur <vshagur@gmail.com>
2021-02-26 13:44:39 +0700

    [tests] add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
-
+from gino import GinoEngine
 from server.api.main import get_app, get_db_config, set_db_client
+import functools
 
 
 @pytest.fixture()
@@ -9,3 +10,45 @@ def app():
     db_config = get_db_config()
     set_db_client(app, db_config)
     return app
+
+
+@pytest.fixture
+async def api_test_client(test_client, app):
+    client = await test_client(app)
+    yield client
+
+# @pytest.fixture(autouse=True)
+# async def db_transaction(api_test_client):
+#
+#     db = api_test_client.app["db_client"].db
+#     real_acquire = GinoEngine.acquire
+#
+#     async with db.acquire() as conn:
+#
+#         class _AcquireContext:
+#             __slots__ = ["_acquire", "_conn"]
+#
+#             def __init__(self, acquire):
+#                 self._acquire = acquire
+#
+#             async def __aenter__(self):
+#                 return conn
+#
+#             async def __aexit__(self, exc_type, exc_val, exc_tb):
+#                 pass
+#
+#             def __await__(self):
+#                 return conn
+#
+#         def acquire(
+#             self, *, timeout=None, reuse=False, lazy=False, reusable=True
+#         ):
+#             return _AcquireContext(
+#                 functools.partial(self._acquire, timeout, reuse, lazy, reusable)
+#             )
+#
+#         GinoEngine.acquire = acquire
+#         transaction = await conn.transaction()
+#         yield
+#         await transaction.rollback()
+#         GinoEngine.acquire = real_acquire
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,26 +1,19 @@
 from db.models import Document
 from marshmallow import ValidationError
-from server.api.schemas import QuestionSchemaResponse
+from server.api.schemas import (QuestionSchemaResponse, TopSchemaResponse, )
 
 
-async def test_get_help(test_client, app):
-    client = await test_client(app)
-    resp = await client.get('/docs/help')
-
+async def test_get_help(api_test_client):
+    resp = await api_test_client.get('/docs/help')
     assert resp.status == 200
-
     data = await resp.json()
     expected = await Document.query.where(Document.name == 'help').gino.first()
-
     assert data.get('text') == expected.text
 
 
-async def test_get_question_random_return_valid_schema(test_client, app):
-    client = await test_client(app)
-    resp = await client.get('/question')
-
+async def test_get_question_random_return_valid_schema(api_test_client):
+    resp = await api_test_client.get('/question')
     assert resp.status == 200
-
     data = await resp.json()
 
     try:
@@ -28,3 +21,32 @@ async def test_get_question_random_return_valid_schema(test_client, app):
     except ValidationError as err:
         assert False, f'GET request to /question return response with not valid ' \
                       f'schema \n{err.messages}'
+
+
+async def test_get_top_return_valid_schema(api_test_client):
+    resp = await api_test_client.get('/top')
+    assert resp.status == 200
+    data = await resp.json()
+
+    try:
+        schema = TopSchemaResponse().load(data)
+    except ValidationError as err:
+        assert False, f'GET request to /top return response with not valid ' \
+                      f'schema \n{err.messages}'
+
+
+async def test_post_question(aiohttp_client, app):
+    client = await aiohttp_client(app)
+    data = {
+        "timeout": 3,
+        "correct_id": 1,
+        "answer1_text": "human123456789",
+        "answer3_text": "dog",
+        "question_text": "who are you?",
+        "answer2_text": "cat"
+    }
+    resp = await client.post('/question', json=data)
+    assert resp.status == 200
+    data = await resp.json()
+    assert 'id' in data
+    assert isinstance(data.get('id'), int)
